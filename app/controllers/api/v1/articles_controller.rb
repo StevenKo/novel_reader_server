cc4f59f1e42c82b231724b37d4411d599425b169
Steven Ko <chunyuko85@gmaill.com>
2013-03-04 21:38:51 +0800

    article index order

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::ArticlesController < Api::ApiController
   def index
     novel_id = params[:novel_id]
-    # order = params[:order]
-    articles = Article.where('novel_id = (?)', novel_id).select("id,title,subject").paginate(:page => params[:page], :per_page => 15)
+    order = params[:order]
+
+    if order == "true"
+      articles = Article.where('novel_id = (?)', novel_id).select("id,title,subject").paginate(:page => params[:page], :per_page => 15)
+    else
+      articles = Article.where('novel_id = (?)', novel_id).select("id,title,subject").paginate(:page => params[:page], :per_page => 15).by_id_desc
+    end
+
     render :json => articles
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :novel
+  scope :by_id_desc, order('id DESC')
 end
