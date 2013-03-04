69cf1ac92da4a5d1698851724bbd5e2be2ae02c5
Steven Ko <chunyuko85@gmaill.com>
2013-03-04 17:25:47 +0800

    select article

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::ArticlesController < Api::ApiController
   def index
     novel_id = params[:novel_id]
     # order = params[:order]
-    articles = Article.where('novel_id = (?)', novel_id).select("id,link,title,subject").paginate(:page => params[:page], :per_page => 15)
+    articles = Article.where('novel_id = (?)', novel_id).select("id,title,subject").paginate(:page => params[:page], :per_page => 15)
     render :json => articles
   end
 
   def show
-    article = Article.find(params[:id])
+    article = Article.select("text").find(params[:id])
     render :json => article
   end
 end
