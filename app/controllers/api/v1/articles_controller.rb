cd73df3727cbe823426abd978b6c615a2fa68d00
Steven Ko <chunyuko85@gmaill.com>
2013-03-11 16:01:51 +0800

    reder articles with page

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::ArticlesController < Api::ApiController
     order = params[:order]
 
     if order == "true"
-      articles = Article.where('novel_id = (?)', novel_id).select("id,title,subject").paginate(:page => params[:page], :per_page => 15)
+      articles = Article.where('novel_id = (?)', novel_id).select("id,title,subject")
     else
-      articles = Article.where('novel_id = (?)', novel_id).select("id,title,subject").paginate(:page => params[:page], :per_page => 15).by_id_desc
+      articles = Article.where('novel_id = (?)', novel_id).select("id,title,subject").by_id_desc
     end
 
     render :json => articles
