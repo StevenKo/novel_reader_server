83acee4366b4262ed3fa1b9b24ce517ece57d327
Steven Ko <chunyuko85@gmaill.com>
2013-03-14 23:40:34 +0800

    crawl article text schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 env :PATH, ENV['PATH']
 
-every :day, :at => '01:50pm' do
-  rake 'crawl:crawl_novel_detail_and_articles',:output => {:error => 'log/error.log', :standard => 'log/cron.log'}
+every :day, :at => '11:50pm' do
+  rake 'crawl:crawl_article_text',:output => {:error => 'log/error.log', :standard => 'log/cron.log'}
 end
