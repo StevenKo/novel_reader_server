57cf1e5958a3b8faab21e88f54337002734d260c
Steven Ko <chunyuko85@gmaill.com>
2013-04-27 20:03:17 +0800

    crawl novel

--- app/workers/crawl_new_novel_worker.rb
+++ app/workers/crawl_new_novel_worker.rb
@@ -4,9 +4,10 @@ class CrawlNewNovelWorker
   sidekiq_options queue: "novel"
   
   def perform(category_id, page)
+    cat_hash = { 1=>1, 2=>2, 3=>3,4=>4,5=>8,6=>5,7=>6,8=>9,9=>10,10=>11,11=>14,12=>15}
     begin
       crawler = NovelCrawler.new
-      crawler.fetch "http://www.bestory.com/category/#{category_id}-#{page}.html"
+      crawler.fetch "http://www.bestory.com/category/#{cat_hash[category_id]}-#{page}.html"
       crawler.crawl_novels category_id
     rescue
       puts category.name + ":  http://www.bestory.com/category/#{category_id}-#{page}.html"
