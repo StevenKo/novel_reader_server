69113f2ae03082b0634aa71378f10211e18ace3f
Steven Ko <chunyuko85@gmaill.com>
2013-05-08 12:44:35 +0800

    zizaidu

--- lib/classes/novel_crawler.rb
+++ lib/classes/novel_crawler.rb
@@ -205,6 +205,12 @@ class NovelCrawler
       article_text = ZhConv.convert("zh-tw", text)
       article.text = article_text
       article.save
+    elsif(@page_url.index("www.zizaidu.com/big5"))
+      nodes = @page_html.css("#content")
+      text  = change_node_br_to_newline(nodes).strip
+      article_text = text.gsub("(最好的全文字小說網︰自在讀小說網 www.zizaidu.com)","")
+      article.text = article_text
+      article.save
     end
   end
 
