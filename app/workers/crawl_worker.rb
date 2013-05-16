364a8846f4a64b160b3f8227c5641683e8721a87
Steven Ko <chunyuko85@gmaill.com>
2013-05-17 00:23:19 +0800

    crawl novel articles other site

--- app/workers/crawl_worker.rb
+++ app/workers/crawl_worker.rb
@@ -4,10 +4,16 @@ class CrawlWorker
   sidekiq_options queue: "novel"
   
   def perform(novel_id)
-    novel = Novel.select("id, link").find(novel_id)
+    novel = Novel.select("id, link, is_show").find(novel_id)
+    return if novel.is_show == false
+
     crawler = NovelCrawler.new
-    crawler.fetch novel.link
-    crawler.crawl_novel_detail novel.id
+    if(novel.link.index('bestory'))
+      crawler.fetch novel.link
+      crawler.crawl_novel_detail novel.id
+    else
+      crawler.fetch_other_site novel.link
+    end
     crawler.crawl_articles novel.id
     puts novel.id
   end
