c2eabd99839dfa862db4bf61c45d1905700f96ee
Steven Ko <chunyuko85@gmaill.com>
2013-07-27 18:55:26 +0800

    fix controller error

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    regid = params[:regid]
+    user = User.find_by_registration_id(regid)
+    if user
+      user.registration_id = regid;
+      user.save
+    else
+      user = User.new
+      user.registration_id = regid
+      user.read_novels = []
+      user.save
+    end
+    render :status=>200, :json => {"message" => "success"}
+  end
+
+  def update_novel
+    novel = params[:novel]
+    regid = params[:regid]
+    user = User.find_by_registration_id(regid)
+    if user
+      user.read_novels << novel unless user.read_novels.include? novel
+      user.save
+      render :status=>200, :json => {"message" => "success"}
+    else
+      render :status=>404, :json => {"message" => "fail"}
+    end
+    
+  end
+end
