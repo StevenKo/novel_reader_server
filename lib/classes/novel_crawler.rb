83a6202b61e88c00a439565959e2106840b39cae
Steven Ko <chunyuko85@gmaill.com>
2013-08-15 15:36:46 +0800

    update sfacg

--- lib/classes/novel_crawler.rb
+++ lib/classes/novel_crawler.rb
@@ -854,6 +854,8 @@ class NovelCrawler
         nodes = @page_html.css(".list_Content")[index].css("a")
         nodes.each do |node|
             article = Article.find_by_link("http://book.sfacg.com" + node[:href])
+            article.subject = subject_titles[index]
+            article.save
             next if (article != nil && article.text != nil)
 
             unless article 
@@ -1312,6 +1314,27 @@ class NovelCrawler
         end
         ArticleWorker.perform_async(article.id)
       end
+    elsif(@page_url.index('du7.com'))
+      nodes = @page_html.css(".uclist a")
+      nodes.each do |node|
+        article = Article.find_by_link(@page_url + node[:href])
+        next if (article != nil && article.text != nil)
+
+        unless article 
+          article = Article.new
+          article.novel_id = novel_id
+          article.link = @page_url + node[:href]
+          article.title = ZhConv.convert("zh-tw",node.text.strip)
+          novel = Novel.select("id,num,name").find(novel_id)
+          article.subject = novel.name
+          article.num = novel.num + 1
+          novel.num = novel.num + 1
+          novel.save
+          # puts node.text
+          article.save
+        end
+        ArticleWorker.perform_async(article.id)
+      end
     elsif(@page_url.index('lwxs'))
       url = @page_url
       nodes = @page_html.css("div#defaulthtml4 td a")
@@ -1457,7 +1480,36 @@ class NovelCrawler
             ArticleWorker.perform_async(article.id)
           end
         end
-      end  
+      end
+    elsif(@page_url.index('zwwx.com'))
+      subject = ""
+      nodes = @page_html.css(".book_article_texttable div")
+      nodes.each do |node|
+        if node[:class] == "book_article_texttitle"
+          subject = ZhConv.convert("zh-tw",node.text.strip)
+        else
+          inside_nodes = node.css("a")
+          inside_nodes.each do |in_node|
+            article = Article.find_by_link(in_node[:href])
+            next if (article != nil && article.text != nil && article.text.length > 100)
+
+            unless article 
+              article = Article.new
+              article.novel_id = novel_id
+              article.link = in_node[:href]
+              article.title = ZhConv.convert("zh-tw",in_node.text.strip)
+              novel = Novel.select("id,num,name").find(novel_id)
+              article.subject = subject
+              article.num = novel.num + 1
+              novel.num = novel.num + 1
+              novel.save
+              # puts node.text
+              article.save
+            end
+            ArticleWorker.perform_async(article.id)
+          end
+        end
+      end
     elsif(@page_url.index('xs8.cn'))
       url = ""
       nodes = @page_html.css("div.mod_container a")
@@ -2918,6 +2970,16 @@ class NovelCrawler
       node = @page_html.css(".t_f")
       text = node.text.strip
       article.text = text
+      article.save
+    elsif(@page_url.index('zwwx.com'))
+      node = @page_html.css("#content")
+      text = node.text.strip
+      article.text = ZhConv.convert("zh-tw", text.strip)
+      article.save
+    elsif(@page_url.index('du7.com'))
+      node = @page_html.css(".text")
+      text = change_node_br_to_newline(node).strip
+      article.text = ZhConv.convert("zh-tw", text.strip)
       article.save
     end
   end
