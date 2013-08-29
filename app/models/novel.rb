0ddd47d0c55b245fb4b7c2ccd7d35359da8b728d
Steven Ko <chunyuko85@gmaill.com>
2013-08-29 11:19:50 +0800

    mass assign link

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,5 +1,5 @@
 class Novel < ActiveRecord::Base
-  attr_accessible :name, :author, :description, :pic, :category_id, :article_num, :last_update, :is_serializing, :is_category_recommend, :is_category_hot, :is_category_this_week_hot, :is_classic, :is_classic_action, :is_show
+  attr_accessible :name, :author, :description, :pic, :category_id, :article_num, :last_update, :is_serializing, :is_category_recommend, :is_category_hot, :is_category_this_week_hot, :is_classic, :is_classic_action, :is_show, :link
   belongs_to :category
   has_many :articles
 
