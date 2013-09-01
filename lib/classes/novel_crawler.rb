dea5ec6d91dfc9b52a01d3285eb92425a451f02d
Steven Ko <chunyuko85@gmaill.com>
2013-09-01 09:16:27 +0800

    add 250xy

--- lib/classes/novel_crawler.rb
+++ lib/classes/novel_crawler.rb
@@ -3475,6 +3475,20 @@ class NovelCrawler
       text = change_node_br_to_newline(node)
       article.text = ZhConv.convert("zh-tw", text.strip)
       article.save
+    elsif(@page_url.index('250sy'))
+      node = @page_html.css("#contents")
+      text = change_node_br_to_newline(node)
+      article.text = ZhConv.convert("zh-tw", text.strip)
+      if text.length < 100
+        imgs = @page_html.css("#content .divimage img")
+        text_img = ""
+        imgs.each do |img|
+            text_img = text_img + img[:src] + "*&&$$*"
+        end
+        text_img = text_img + "如果看不到圖片, 請更新至新版APP"
+        article.text = text_img
+      end
+      article.save
     end
   end
 
