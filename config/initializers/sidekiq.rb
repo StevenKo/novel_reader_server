dd11e09cb94961ba6c4920e26ba03e4b5754db36
Steven Ko <chunyuko85@gmail.com>
2013-09-05 23:14:22 +0800

    sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { :url => 'redis://106.187.97.63:6379', :namespace => 'mynamespace' }
-# end
+Sidekiq.configure_server do |config|
+  config.redis = { :url => 'redis://106.187.97.63:6379', :namespace => 'mynamespace' }
+end
 
-# Sidekiq.configure_client do |config|
-#   config.redis = { :url => 'redis://106.187.97.63:6379', :namespace => 'mynamespace' }
-# end
+Sidekiq.configure_client do |config|
+  config.redis = { :url => 'redis://106.187.97.63:6379', :namespace => 'mynamespace' }
+end
