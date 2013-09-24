5dbb43e8c95659b4be50a607d761778a5fb39cd1
Steven Ko <chunyuko85@gmail.com>
2013-09-24 23:27:19 +0800

    disable user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,59 +1,63 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    render :status=>404, :json => {"message" => "fail"} and return unless params[:device_id]
-    user = find_user
-    if user
-      user.registration_id = params[:regid]
-    else
-      user = User.new
-      user.device_id = params[:device_id]
-      user.registration_id = params[:regid]
-      user.read_novels = []
-    end
-    user.country = params[:country] if params[:country]
-    user.platform = params[:platform] if params[:platform]
-    user.version = params[:version] if params[:version]
-    if user.save
-      render :status=>200, :json => {"message" => "success"}
-    else
-      render :status=>404, :json => {"message" => "fail"}
-    end
+    render :status=>404, :json => {"message" => "fail"}
+    # render :status=>404, :json => {"message" => "fail"} and return unless params[:device_id]
+    # user = find_user
+    # if user
+    #   user.registration_id = params[:regid]
+    # else
+    #   user = User.new
+    #   user.device_id = params[:device_id]
+    #   user.registration_id = params[:regid]
+    #   user.read_novels = []
+    # end
+    # user.country = params[:country] if params[:country]
+    # user.platform = params[:platform] if params[:platform]
+    # user.version = params[:version] if params[:version]
+    # if user.save
+    #   render :status=>200, :json => {"message" => "success"}
+    # else
+    #   render :status=>404, :json => {"message" => "fail"}
+    # end
   end
 
   def update_novel
-    novel = params[:novel]
-    user = find_user
-    if user
-      user.read_novels << novel unless user.read_novels.include? novel
-      user.save
-      render :status=>200, :json => {"message" => "success"}
-    else
-      render :status=>404, :json => {"message" => "fail"}
-    end 
+    render :status=>404, :json => {"message" => "fail"}
+    # novel = params[:novel]
+    # user = find_user
+    # if user
+    #   user.read_novels << novel unless user.read_novels.include? novel
+    #   user.save
+    #   render :status=>200, :json => {"message" => "success"}
+    # else
+    #   render :status=>404, :json => {"message" => "fail"}
+    # end 
   end
 
   def update_collected_novels
-    novels = params[:novels]
-    user = find_user
-    if user
-      user.collected_novels = novels
-      user.save
-      render :status=>200, :json => {"message" => "success"}
-    else
-      render :status=>404, :json => {"message" => "fail"}
-    end
+    render :status=>200, :json => {"message" => "success"}
+    # novels = params[:novels]
+    # user = find_user
+    # if user
+    #   user.collected_novels = novels
+    #   user.save
+    #   render :status=>200, :json => {"message" => "success"}
+    # else
+    #   render :status=>404, :json => {"message" => "fail"}
+    # end
   end
 
   def update_downloaded_novels
-    novels = params[:novels]
-    user = find_user
-    if user
-      user.downloaded_novels = novels
-      user.save
-      render :status=>200, :json => {"message" => "success"}
-    else
-      render :status=>404, :json => {"message" => "fail"}
-    end
+    render :status=>200, :json => {"message" => "success"}
+    # novels = params[:novels]
+    # user = find_user
+    # if user
+    #   user.downloaded_novels = novels
+    #   user.save
+    #   render :status=>200, :json => {"message" => "success"}
+    # else
+    #   render :status=>404, :json => {"message" => "fail"}
+    # end
   end
 
   private
