25f46ebf3a0da084ecab04d16d028424edf2aabd
Steven Ko <chunyuko85@gmail.com>
2013-09-28 08:58:30 +0800

    article show admin

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ActiveRecord::Base
   scope :by_num_asc, order('num ASC')
   scope :show, where(:is_show => true)
   has_one :article_text
-  delegate :text, to: :article_text, prefix: "article_all"
+  delegate :text, to: :article_text, prefix: "article_all", :allow_nil => true
 
   scope :novel_articles, lambda { |novel_id| where('novel_id = (?)', novel_id).select('id') }
 
