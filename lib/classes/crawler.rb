c8d36b0d1a63ee477297e80a26fcfacb0f197d6a
Steven Ko <chunyuko85@gmail.com>
2013-09-28 10:16:21 +0800

    crawler force encode

--- lib/classes/crawler.rb
+++ lib/classes/crawler.rb
@@ -71,6 +71,7 @@ module Crawler
       body.encode!("utf-8", :undef => :replace, :replace => "?", :invalid => :replace)
       @page_html = Nokogiri::HTML(body,nil)
     else
+      body.encode!("utf-8", :undef => :replace, :replace => "?", :invalid => :replace)
       @page_html = Nokogiri::HTML(body)
     end
   end
