c03a82361bd1e3b7093e34c672f591a0eacecc4b
Steven Ko <chunyuko85@gmail.com>
2013-11-02 11:16:00 +0800

    crawl fix shumilou.rb

--- lib/classes/crawler/shumilou.rb
+++ lib/classes/crawler/shumilou.rb
@@ -33,6 +33,17 @@ class Crawler::Shumilou
     text = @page_html.css("#content").text.strip
     article_text = ZhConv.convert("zh-tw",text)
     text = article_text
+
+    if text.size < 100
+      imgs = @page_html.css("#content img")
+      text_img = ""
+      imgs.each do |img|
+          text_img = text_img + img[:src] + "*&&$$*"
+      end
+      text_img = text_img + "如果看不到圖片, 請更新至新版APP"
+      text = text_img
+    end
+
     raise 'Do not crawl the article text ' unless isArticleTextOK(article,text)
     ArticleText.update_or_create(article_id: article.id, text: text)
   end
