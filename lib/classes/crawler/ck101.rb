106f2a5946dad2361ba031ade7e43c9b54e01d37
Steven Ko <chunyuko85@gmail.com>
2014-02-17 15:53:16 +0800

    crawler

--- lib/classes/crawler/ck101.rb
+++ lib/classes/crawler/ck101.rb
@@ -15,6 +15,9 @@ class Crawler::Ck101
         url = "http://ck101.com/" + "thread-#{$1}-#{page}-2.html"
       end
       article = Article.select("articles.id, is_show, title, link, novel_id, subject, num").find_by_link(url)
+      if novel_id == 1680 && article
+        ArticleWorker.perform_async(article.id)
+      end
       next if article
 
       unless article 
--- lib/classes/crawler/wanshuba.rb
+++ lib/classes/crawler/wanshuba.rb
@@ -3,7 +3,7 @@ class Crawler::Wanshuba
   include Crawler
 
   def crawl_article article
-    imgs = @page_html.css("img#imgbook")
+    imgs = @page_html.css("#content img")
     text_img = ""
     imgs.each do |img|
         text_img = text_img + "http://www.wanshuba.com" + img[:src] + "*&&$$*"
