8abe3a930d706f7021fc29e562d27326750d95c0
Steven Ko <chunyuko85@gmail.com>
2014-07-10 12:11:20 +0800

    article new set num

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,22 +26,7 @@ class ArticlesController < ApplicationController
     novel_id = params[:novel_id]
     article_id = params[:article_id]
 
-    article = Article.where("novel_id = #{novel_id} and num = #{num}")
-    if article[0]
-      articles = Article.select("id,num").where("novel_id = #{novel_id} and num >= #{num}")
-      Article.transaction do
-        articles.each do |a|
-          a.update_column(:num,a.num + 1)
-        end
-      end
-      novel = Novel.select("id,num").find(novel_id)
-      novel.update_column(:num,novel.num + 1)
-      article = Article.select("id,num").find(article_id)
-      article.update_column(:num,num)
-    else
-      article = Article.select("id,num").find(article_id)
-      article.update_column(:num,num)
-    end
+    reset_articles_num(novel_id, num, article_id)
     
     redirect_to :controller => 'novels', :action => 'show', :id => novel_id, :page => params[:page]
   end
@@ -56,18 +41,23 @@ class ArticlesController < ApplicationController
 
   def create
     text = params[:article][:article_all_text]
+    num = params[:article][:num]
     params[:article].delete(:article_all_text)
+    params[:article].delete(:num)
+
     article = Article.new(params[:article])
     novel = Novel.select("id,num").find(article.novel_id)
     article.num = novel.num + 1
     novel.num = novel.num + 1
+
     if article.save && novel.save
+      reset_articles_num(novel.id, num, article.id) if num
       page_html = Nokogiri::HTML(text)
       page_html.css("font,span").remove
       text = page_html.text
 
       ArticleText.create(article_id: article.id, text: text)
-      redirect_to article_path(article.id)
+      redirect_to novel_path(novel.id, page: params[:page][:page])
     else
       render :action => "new", :novel_id => article.novel_id
     end
@@ -101,5 +91,26 @@ class ArticlesController < ApplicationController
   def search_by_num
     @article = Article.where("novel_id = #{params[:novel_id]} and num = #{params[:num]}")[0]
     render :show
+  end
+
+  private
+
+  def reset_articles_num(novel_id, num, article_id)
+    article = Article.where("novel_id = #{novel_id} and num = #{num}")
+    if article[0]
+      articles = Article.select("id,num").where("novel_id = #{novel_id} and num >= #{num}")
+      Article.transaction do
+        articles.each do |a|
+          a.update_column(:num,a.num + 1)
+        end
+      end
+      novel = Novel.select("id,num").find(novel_id)
+      novel.update_column(:num,novel.num + 1)
+      article = Article.select("id,num").find(article_id)
+      article.update_column(:num,num)
+    else
+      article = Article.select("id,num").find(article_id)
+      article.update_column(:num,num)
+    end
   end
 end
