4cebad0bfdc2c443259175ac8d5717af8e01feb7
Steven Ko <chunyuko85@gmail.com>
2014-10-14 16:04:05 +0800

    fix recrawl worker

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -9,7 +9,7 @@ class Novel < ActiveRecord::Base
     Article.where("novel_id = #{id} and is_show = true").select("id").find_in_batches(:batch_size => 100) do |articles|
       articles.each do |article|
         texts = ArticleText.select("id").where("article_id = #{article.id}")
-        CapybaraArticleWorker.perform_async(article.id) unless texts.present?
+        ArticleWorker.perform_async(article.id) unless texts.present?
       end 
     end
   end
