4a685bd35abb8301a458e5ad25adbcecb68b9523
Steven Ko <chunyuko85@gmail.com>
2014-11-06 11:57:00 +0800

    app version

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -37,7 +37,7 @@ class Api::ApiController  < ActionController::Base
   end
 
   def version_check
-    render :status=>200, :json=>{ version: 62, update_link: "https://play.google.com/store/apps/details?id=com.novel.reader"}
+    render :status=>200, :json=>{ version: 63, update_link: "https://play.google.com/store/apps/details?id=com.novel.reader"}
   end
 
 end
