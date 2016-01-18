897c49d9da573189c4982f892c21890b72f554c9
Steven Ko <chunyuko85@gmail.com>
2016-01-18 19:01:23 +0800

    snwx

--- lib/classes/crawler/snwx.rb
+++ lib/classes/crawler/snwx.rb
@@ -45,6 +45,15 @@ class Crawler::Snwx
     node.css("script").remove
     text = change_node_br_to_newline(node).strip
     text = ZhConv.convert("zh-tw", text.strip,false)
+    if text.size < 100
+      imgs = @page_html.css(".divimage img")
+      text_img = ""
+      imgs.each do |img|
+          text_img = text_img + img[:src] + "*&&$$*"
+      end
+      text_img = text_img + "如果看不到圖片, 請更新至新版"
+      text = text_img
+    end
     raise 'Do not crawl the article text ' unless isArticleTextOK(article,text)
     ArticleText.update_or_create(article_id: article.id, text: text)
   end
