e3cc8ae1ac43413df902792bbbf366c6841c5d81
Steven Ko <chunyuko85@gmail.com>
2016-01-28 19:52:58 +0800

    fix qbxs8

--- lib/classes/crawler/qbxs8.rb
+++ lib/classes/crawler/qbxs8.rb
@@ -3,8 +3,6 @@ class Crawler::Qbxs8
   include Crawler
 
   def crawl_articles novel_id
-    url = @page_url.sub("index.shtml","")
-    url = @page_url.sub("Index.shtml","")
     nodes = @page_html.css("ul li a")
     do_not_crawl_from_link = true
     from_link = (FromLink.find_by_novel_id(novel_id).nil?) ? nil : FromLink.find_by_novel_id(novel_id).link
@@ -12,13 +10,13 @@ class Crawler::Qbxs8
       do_not_crawl_from_link = false if crawl_this_article(from_link,node[:href])
       next if do_not_crawl_from_link
       
-      article = Article.select("articles.id, is_show, title, link, novel_id, subject, num").find_by_link(url + node[:href])
+      article = Article.select("articles.id, is_show, title, link, novel_id, subject, num").find_by_link(get_article_url(node[:href]))
       next if article
 
       unless article 
         article = Article.new
         article.novel_id = novel_id
-        article.link = url + node[:href]
+        article.link = get_article_url(node[:href])
         article.title = ZhConv.convert("zh-tw",node.text.strip,false)
         novel = Novel.select("id,num,name").find(novel_id)
         article.subject = novel.name
