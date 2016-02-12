8192015cf759f509b21a50b3585320d54a65a84b
Steven Ko <chunyuko85@gmail.com>
2016-02-12 15:39:46 +0800

    crawler

--- lib/classes/crawler/dz88.rb
+++ lib/classes/crawler/dz88.rb
@@ -1,23 +1,22 @@
 # encoding: utf-8
-class Crawler::Wuqingshui
+class Crawler::Dz88
   include Crawler
 
   def crawl_articles novel_id
-    nodes = @page_html.css(".list-group-item.book a")
+    nodes = @page_html.css(".entry_video_list a")
     do_not_crawl_from_link = true
     from_link = (FromLink.find_by_novel_id(novel_id).nil?) ? nil : FromLink.find_by_novel_id(novel_id).link
     nodes.each do |node|      
       do_not_crawl_from_link = false if crawl_this_article(from_link,node[:href])
       next if do_not_crawl_from_link
 
-      article_url = get_article_url(node[:href])
-      article = Article.select("articles.id, is_show, title, link, novel_id, subject, num").find_by_link(article_url)
+      article = Article.select("articles.id, is_show, title, link, novel_id, subject, num").find_by_link(get_article_url(node[:href]))
       next if article
 
       unless article 
         article = Article.new
         article.novel_id = novel_id
-        article.link = article_url
+        article.link = get_article_url(node[:href])
         article.title = ZhConv.convert("zh-tw",node.text.strip,false)
         novel = Novel.select("id,num,name").find(novel_id)
         article.subject = novel.name
@@ -33,8 +32,8 @@ class Crawler::Wuqingshui
   end
 
   def crawl_article article
-    node = @page_html.css(".content")
-    node.css("script").remove
+    node = @page_html.css("#view_content_txt")
+    node.css('script,a').remove
     text = change_node_br_to_newline(node).strip
     text = ZhConv.convert("zh-tw", text.strip, false)
     raise 'Do not crawl the article text ' unless isArticleTextOK(article,text)
