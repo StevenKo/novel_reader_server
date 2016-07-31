b3aa4883ec92083bf74191df7a86a4a91d583d63
Steven Ko <chunyuko85@gmail.com>
2016-07-31 11:18:35 +0800

    dmzj.rb

--- lib/classes/crawler/dmzj.rb
+++ lib/classes/crawler/dmzj.rb
@@ -75,4 +75,27 @@ class Crawler::Dmzj
     return text,links
   end
 
+  def crawl_novel(category_id)
+    img_link = @page_html.css(".novel_cover img")[0][:src]
+    name = node.css(".novel_cover_text h1")[0].text
+    is_serializing = false
+    is_serializing = true if @page_html.css(".spanwidth1").text.index("连载中")
+    author = @page_html.css("span.spanwidth1 l1")[0].text.gsub("作者：","")
+    description = change_node_br_to_newline(node.css("#show_intro").next.text).strip
+    link = @page_url
+    
+    novel = Novel.new
+    novel.link = link
+    novel.name = ZhConv.convert("zh-tw",name,false)
+    novel.author = ZhConv.convert("zh-tw",author,false)
+    novel.category_id = category_id
+    novel.is_show = true
+    novel.is_serializing = is_serializing
+    novel.last_update = Time.now.strftime("%m/%d/%Y")
+    novel.article_num = "?"
+    novel.description = description
+    novel.save
+    CrawlWorker.perform_async(novel.id)
+  end
+
 end
