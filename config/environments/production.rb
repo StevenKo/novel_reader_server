03afbadc0a717f70e2b8a525d767d1f99f76d101
Steven Ko <chunyuko85@gmail.com>
2016-11-11 21:36:09 +0800

    modify production log size

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ NovelServer::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.logger = Logger.new(config.paths["log"].first, 50, 1.megabyte)
+  config.logger = Logger.new(config.paths["log"].first, 10, 100.megabyte)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
